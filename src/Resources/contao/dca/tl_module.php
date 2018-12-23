feb2ab2091a52286021142464aed87eb08771f55
intelligentspark <fred.bliss@intelligentspark.com>
2018-12-22 23:50:11 -0500

    second commit additional files, removed unusued files

---
+++ src/Resources/contao/dca/tl_module.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2016 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+
+/**
+ * Add palettes to tl_module
+ */
+$GLOBALS['TL_DCA']['tl_module']['palettes']['petango_details'] = '{title_legend},name,headline,type;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
